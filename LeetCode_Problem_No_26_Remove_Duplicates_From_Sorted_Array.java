c87d000482ecfa5423ace3eb68f2589580d170b0
MsL-Mishal <106803397+MsL-Mishal@users.noreply.github.com>
2024-05-29 23:49:24 +0530

    Create LeetCode_Problem_No_26_Remove_Duplicates_From_Sorted_Array.java
    Java Solution of Leet Code Probelm Number 26 - Remove Duplicates from Sorted Array

---
+++ LeetCode_Problem_No_26_Remove_Duplicates_From_Sorted_Array.java
@@ -0,0 +1,30 @@
+class Solution 
+{
+    public int removeDuplicates(int[] nums) 
+    {
+        // Two Pointer Approach
+        int prev = Integer.MIN_VALUE;
+        int traversalPtr = 0;  // Pointer to traverse the entire array.
+        int k = 0;  // Pointer to keep track of indexes at correct positions while duplicate elements are pushed towards the rightmost end.
+
+        // Keep in mind that this is a sorted array.
+        while( traversalPtr < nums.length )
+        {
+            int currentElement = nums[traversalPtr];
+
+            // If we have a duplicate element, we won't update `k` value. We'll only update `traversalPtr` value.
+            // Else, we'll increment `k` and set current element as `prev` for the next iteration. We'll also update `traversalPtr` value.
+          
+            if( currentElement != prev )
+            {
+                nums[k] = currentElement;
+                k++;
+                prev = currentElement;
+            }
+            traversalPtr++;
+        }
+        return k;  //Now, k will be pointing to the element after which all the elements are duplicates. 
+                    // One thing to note is that we're not deleting the duplicate elements, but only returning index till which no duplicates are present after rearranging the matrix
+                    // such that all the duplicate elements are pushed towards the end.
+    }
+}
