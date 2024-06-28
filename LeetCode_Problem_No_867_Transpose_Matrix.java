974deecdae6fdc554ee714e11771b7a24ed92e6a
MsL-Mishal <106803397+MsL-Mishal@users.noreply.github.com>
2024-06-28 23:44:28 +0530

    Java Solution of Leet Code Problem Number 867 - Transpose Matrix
    Problem Description : 
    
    Given a 2D integer array matrix, return the transpose of matrix.
    
    The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.

---
+++ LeetCode_Problem_No_867_Transpose_Matrix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) 
+    {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        int output[][] = new int[c][r];
+
+        for(int i = 0; i < r; i++)
+            for(int j = 0; j < c; j++)
+                output[j][i] = matrix[i][j];
+            
+        return output;
+    }
+}
