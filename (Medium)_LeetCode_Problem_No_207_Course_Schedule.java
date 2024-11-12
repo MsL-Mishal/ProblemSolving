3b547f2fa27141270af52b426e1cc4db68062c25
MsL-Mishal <106803397+MsL-Mishal@users.noreply.github.com>
2024-11-13 01:24:42 +0530

    Java Solution of (Medium) Leet Code Problem Number 207 - Course Schedule
    Problem Description : 
    
    There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
    
    For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
    Return true if you can finish all courses. Otherwise, return false.
    
     
    Example 1:
    
    Input: numCourses = 2, prerequisites = [[1,0]]
    Output: true
    Explanation: There are a total of 2 courses to take. 
    To take course 1 you should have finished course 0. So it is possible.
    
    Example 2:
    
    Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
    Output: false
    Explanation: There are a total of 2 courses to take. 
    To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
     
    
    Constraints:
    
    1 <= numCourses <= 2000
    0 <= prerequisites.length <= 5000
    prerequisites[i].length == 2
    0 <= ai, bi < numCourses
    All the pairs prerequisites[i] are unique.

---
+++ (Medium)_LeetCode_Problem_No_207_Course_Schedule.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        int indegree[] = new int[numCourses];
+
+        for(int dep[] : prerequisites)
+        {
+            int node1 = dep[0];
+            int node2 = dep[1];
+            adj.computeIfAbsent(node2, f -> new ArrayList<>()).add(node1);
+            indegree[node1] ++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < numCourses; i ++)
+            if(indegree[i] == 0)
+                q.add(i);
+
+        int cc = 0;
+        while(!q.isEmpty())
+        {
+            int node = q.poll();
+            cc ++;
+
+            for(int adjNode: adj.computeIfAbsent(node, f -> new ArrayList<>()))
+            {
+                indegree[adjNode] --;
+                if(indegree[adjNode] == 0)
+                    q.add(adjNode);
+            }
+        }
+
+        return cc == numCourses;
+    }
+}
